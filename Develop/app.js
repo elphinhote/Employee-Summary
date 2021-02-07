801da62ebfba856e0920be6c60f30a8a6981d673
Eva Wang <65749636+elphinhote@users.noreply.github.com>
2021-02-06 16:17:07 -0800

    Add files via upload

---
+++ Develop/app.js
@@ -0,0 +1,115 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const path = require("path");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const render = require("./lib/htmlRenderer");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// create questions object array
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is your role?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    // ask when Manager is chosen for the role
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office number?",
+    when: function (answers) {
+      return answers.role === "Manager";
+    },
+  },
+  {
+    // ask when Engineer is chosen for the role
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+    when: function (answers) {
+      return answers.role === "Engineer";
+    },
+  },
+  {
+    // ask when Intern is chosen for the role
+    type: "input",
+    name: "school",
+    message: "Where do you go to school?",
+    when: function (answers) {
+      return answers.role === "Intern";
+    },
+  },
+  {
+    // ask if user wants to input more employees
+    type: "confirm",
+    name: "again",
+    message: "Add another Employee?",
+    default: true,
+  },
+];
+
+// helper function to recursively prompt the user to add more employees.
+async function createEmployees(employeesInput = []) {
+  try {
+    const { again, ...answers } = await inquirer.prompt(questions);
+    // newEmployee that takes in the employeesInput array called in the function and the answers from the prompt
+    const newEmployee = [...employeesInput, answers];
+    // if user wants to add more employees, call createEmployees function. if not, return newEmployee
+    return again ? createEmployees(newEmployee) : newEmployee;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function init() {
+  try {
+    const employees = [];
+    const employeesData = await createEmployees();
+
+    // map through the employees data and create an object based on the role
+    employeesData.map((employee) => {
+      const { name, id, email, role, officeNumber, github, school } = employee;
+
+      if (role === "Manager") {
+        const newManager = new Manager(name, id, email, officeNumber);
+        employees.push(newManager);
+
+      } else if (role === "Engineer") {
+        const newEngineer = new Engineer(name, id, email, github);
+        employees.push(newEngineer);
+
+      } else {
+        const newIntern = new Intern(name, id, email, school);
+        employees.push(newIntern);
+      }
+    });
+
+    // call render function to take in employees array as a parameter rite the renderedEmployee data to the path team.html page
+    const renderEmployee = render(employees);
+    fs.writeFile(outputPath, renderEmployee, () => console.log("HURRAY!"));
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+init();
