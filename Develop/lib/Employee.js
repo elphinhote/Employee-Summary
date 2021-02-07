a29b8963b211249b96550a2ace8ca2c0a948b456
Eva Wang <65749636+elphinhote@users.noreply.github.com>
2021-02-07 15:16:05 -0800

    Update Employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  // getName/getId/getEmail/getRole fucntions that return name, Id, email
+  getName() {
+    return this.name;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getRole() {
+    return "Employee";
+  }
+}
+
+// export Employee class
+module.exports = Employee;
